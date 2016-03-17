0b0cebe454938ec96649d2a0dd2cc43c6ba1f9b8
qinchuzhou <313802318@qq.com>
2016-03-17 21:50:27 +0800

    新增切换城市和手动更新天气的功能。

--- src/com/qczcoolweather/app/activity/WeatherActivity.java
+++ src/com/qczcoolweather/app/activity/WeatherActivity.java
@@ -65,8 +65,8 @@ public class WeatherActivity extends Activity implements OnClickListener{
 		temp1Text = (TextView) findViewById(R.id.temp1);
 		temp2Text = (TextView) findViewById(R.id.temp2);
 		currentDateText = (TextView) findViewById(R.id.current_date);
-//		switchCity = (Button) findViewById(R.id.switch_city);
-//		refreshWeather = (Button) findViewById(R.id.refresh_weather);
+		switchCity = (Button) findViewById(R.id.switch_city);
+		refreshWeather = (Button) findViewById(R.id.refresh_weather);
 		String countyCode = getIntent().getStringExtra("county_code");
 		if (!TextUtils.isEmpty(countyCode)) {
 			// 有县级代号时就去查询天气
@@ -78,29 +78,29 @@ public class WeatherActivity extends Activity implements OnClickListener{
 			// 没有县级代号时就直接显示本地天气
 			showWeather();
 		}
-//		switchCity.setOnClickListener(this);
-//		refreshWeather.setOnClickListener(this);
+		switchCity.setOnClickListener(this);
+		refreshWeather.setOnClickListener(this);
 	}
 	@Override
 	public void onClick(View v) {
-//		switch (v.getId()) {
-//		case R.id.switch_city:
-//			Intent intent = new Intent(WeatherActivity.this, ChooseAreaActivity.class);
-//			intent.putExtra("from_weather_activity", true);
-//			startActivity(intent);
-//			finish();
-//			break;
-//		case R.id.refresh_weather:
-//			publishText.setText("同步中...");
-//			SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-//			String weatherCode = prefs.getString("weather_code", "");
-//			if (!TextUtils.isEmpty(weatherCode)) {
-//				queryWeatherInfo(weatherCode);
-//			}
-//			break;
-//		default:
-//			break;
-//		}
+		switch (v.getId()) {
+		case R.id.switch_city:
+			Intent intent = new Intent(WeatherActivity.this, ChooseAreaActivity.class);
+			intent.putExtra("from_weather_activity", true);
+			startActivity(intent);
+			finish();
+			break;
+		case R.id.refresh_weather:
+			publishText.setText("同步中...");
+			SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+			String weatherCode = prefs.getString("weather_code", "");
+			if (!TextUtils.isEmpty(weatherCode)) {
+				queryWeatherInfo(weatherCode);
+			}
+			break;
+		default:
+			break;
+		}
 	}
 	/**
 	 * 查询县级代号所对应的天气代号。
